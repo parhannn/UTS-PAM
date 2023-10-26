4ff0bc373038643098e6db54b3e45e1990965bfb
parhannn <farhannn58@gmail.com>
2023-10-26 12:50:23 +0700

    Third Revision of UTS progress: Trying to add pass argument data from API to User Detail Fragment, but failed :"

--- app/src/main/java/com/example/pamuts/ui/home/UserDetailFragment.kt
+++ app/src/main/java/com/example/pamuts/ui/home/UserDetailFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pamuts.databinding.UserDetailBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class UserDetailFragment : Fragment() {
 
     private var _binding: UserDetailBinding? = null
+    private lateinit var database : DatabaseReference
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,10 +36,45 @@ class UserDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val name = arguments?.getString(EXTRA_NAME).toString()
         if (arguments != null) {
-            val name = arguments?.getString(EXTRA_NAME)
             binding.textName.text = name
+
+            readData(name)
+
         }
+
+    }
+
+    private fun readData(username: String) {
+        database = FirebaseDatabase.getInstance().getReference("users")
+            database.addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    if (snapshot.exists()) {
+                        for (userSnapshot in snapshot.children) {
+                            val user = userSnapshot.getValue(UserData::class.java)
+
+                            if (user != null && username == user.username) {
+                                val githubUsername = snapshot.child("githubUsername")
+                                val nik = snapshot.child("nik")
+                                val email = snapshot.child("email")
+
+                                binding.textGithubUsername.text = githubUsername.toString()
+                                binding.textNik.text = nik.toString()
+                                binding.textEmail.text = email.toString()
+                            }
+                        }
+                    }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
     }
 
 }
